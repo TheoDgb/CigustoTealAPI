88f4cb6e46223b54a71edfe4d2736b5eb48e06e7
dal gobbo theo <theo.dal_gobbo@edu.univ-fcomte.fr>
2023-05-25 16:26:28 +0200

    test image + hover cards

--- API/produits.js
+++ API/produits.js
@@ -81,9 +81,91 @@ function getELiquidesProduitsColmar(limit, offset, callback) {
     });
 }
 
+function getConcentresProduitsAndelnans(limit, offset, callback) {
+    connection.query(`SELECT
+                          magasin_id,
+                          libelle_fiche,
+                          MIN(description) AS description,
+                          MIN(marque_id) AS marque_id,
+                          MIN(type_saveur_id) AS type_saveur_id,
+                          MIN(contenance_ml_id) AS contenance_ml_id,
+                          GROUP_CONCAT(DISTINCT dosage_nicotine_mg_id) AS dosage_nicotine_mg_id,
+                          MIN(sel_de_nicotine) AS sel_de_nicotine
+                      FROM produits
+                      WHERE magasin_id = 1 AND categorie_id = 3
+                      GROUP BY magasin_id, libelle_fiche;`, (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results);
+    });
+}
+function getConcentresProduitsBessoncourt(limit, offset, callback) {
+    connection.query(`SELECT
+                          magasin_id,
+                          libelle_fiche,
+                          MIN(description) AS description,
+                          MIN(marque_id) AS marque_id,
+                          MIN(type_saveur_id) AS type_saveur_id,
+                          MIN(contenance_ml_id) AS contenance_ml_id,
+                          GROUP_CONCAT(DISTINCT dosage_nicotine_mg_id) AS dosage_nicotine_mg_id,
+                          MIN(sel_de_nicotine) AS sel_de_nicotine
+                      FROM produits
+                      WHERE magasin_id = 2 AND categorie_id = 3
+                      GROUP BY magasin_id, libelle_fiche;`, (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results);
+    });
+}
+function getConcentresProduitsBesancon(limit, offset, callback) {
+    connection.query(`SELECT
+                          magasin_id,
+                          libelle_fiche,
+                          MIN(description) AS description,
+                          MIN(marque_id) AS marque_id,
+                          MIN(type_saveur_id) AS type_saveur_id,
+                          MIN(contenance_ml_id) AS contenance_ml_id,
+                          GROUP_CONCAT(DISTINCT dosage_nicotine_mg_id) AS dosage_nicotine_mg_id,
+                          MIN(sel_de_nicotine) AS sel_de_nicotine
+                      FROM produits
+                      WHERE magasin_id = 3 AND categorie_id = 3
+                      GROUP BY magasin_id, libelle_fiche;`, (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results);
+    });
+}
+function getConcentresProduitsColmar(limit, offset, callback) {
+    connection.query(`SELECT
+                          magasin_id,
+                          libelle_fiche,
+                          MIN(description) AS description,
+                          MIN(marque_id) AS marque_id,
+                          MIN(type_saveur_id) AS type_saveur_id,
+                          MIN(contenance_ml_id) AS contenance_ml_id,
+                          GROUP_CONCAT(DISTINCT dosage_nicotine_mg_id) AS dosage_nicotine_mg_id,
+                          MIN(sel_de_nicotine) AS sel_de_nicotine
+                      FROM produits
+                      WHERE magasin_id = 4 AND categorie_id = 3
+                      GROUP BY magasin_id, libelle_fiche;`, (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results);
+    });
+}
+
 module.exports = {
     getELiquidesProduitsAndelnans,
     getELiquidesProduitsBessoncourt,
     getELiquidesProduitsBesancon,
     getELiquidesProduitsColmar
+    // ,
+    // getConcentresProduitsAndelnans,
+    // getConcentresProduitsBessoncourt,
+    // getConcentresProduitsBesancon,
+    // getConcentresProduitsColmar
 };
